0fabb2a3183d975402bf492305d7c45615761ed0
theodorSchei <43009539+theodorSchei@users.noreply.github.com>
2021-12-02 18:37:14 +0100

    Changed if-mountain to swich case with python 3.10

--- 2021/day2/part2.py
+++ 2021/day2/part2.py
@@ -7,23 +7,23 @@ commands = []
 units = []
 
 for line in lines:
-    commands.append(line.split(' ')[0])
-    units.append(int(line.split(' ')[1].strip()))
+	commands.append(line.split(' ')[0])
+	units.append(int(line.split(' ')[1].strip()))
 
 aim = 0
 horizontal_position = 0
 depth = 0
 
 for command, unit in zip(commands, units):
-
-    if command == 'forward':
-        horizontal_position += unit
-        depth = depth + aim * unit
-
-    elif command == 'up':
-        aim -= unit
-    elif command == 'down':
-        aim += unit
+	
+	match command:
+		case 'forward':
+			horizontal_position += unit
+			depth = depth + aim * unit
+		case 'up':
+			aim -= unit
+		case 'down':
+			aim += unit
 
 print(horizontal_position * depth)
 
