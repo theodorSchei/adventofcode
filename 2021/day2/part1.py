b91d8f7b7cd2b3c61e70ec2a3ec7fcc23f125928
theodorSchei <43009539+theodorSchei@users.noreply.github.com>
2021-12-02 16:02:24 +0100

    finished day 2

---
+++ 2021/day2/part1.py
@@ -0,0 +1,27 @@
+file = open('day2/input.txt', 'r')
+lines = file.readlines()
+
+input = []
+
+commands = []
+units = []
+
+for line in lines:
+    commands.append(line.split(' ')[0])
+    units.append(int(line.split(' ')[1].strip()))
+
+depth = 0
+horizontal_position = 0
+
+for command, unit in zip(commands, units):
+
+    if command == 'forward':
+        horizontal_position += unit
+    elif command == 'up':
+        depth -= unit
+    elif command == 'down':
+        depth += unit
+
+print(horizontal_position * depth)
+
+file.close()
