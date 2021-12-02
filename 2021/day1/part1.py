d1718558afaaa1ed95e311478244cc694dfed0ed
theodorSchei <43009539+theodorSchei@users.noreply.github.com>
2021-12-02 18:52:48 +0100

    tabs vs. spaces fiesta

--- 2021/day1/part1.py
+++ 2021/day1/part1.py
@@ -4,13 +4,13 @@ lines = file.readlines()
 numbers = []
 
 for line in lines:
-    numbers.append(int(line.strip()))
+	numbers.append(int(line.strip()))
 
 total = 0
 
 for previous, current in zip(numbers, numbers[1:]):
-    if(current > previous):
-        total = total + 1
+	if(current > previous):
+		total = total + 1
 
 print(total)
 
--- 2021/day1/part2.py
+++ 2021/day1/part2.py
@@ -4,16 +4,16 @@ lines = file.readlines()
 numbers = []
 
 for line in lines:
-    numbers.append(int(line.strip()))
+	numbers.append(int(line.strip()))
 
 total = 0
 
 for i in range(0, len(numbers)-3, 1):
-    current = numbers[i] + numbers[i+1] + numbers[i+2]
-    next = numbers[i+1] + numbers[i+2] + numbers[i+3]
+	current = numbers[i] + numbers[i+1] + numbers[i+2]
+	next = numbers[i+1] + numbers[i+2] + numbers[i+3]
 
-    if next > current:
-        total = total + 1
+	if next > current:
+		total = total + 1
 
 print(total)
 
