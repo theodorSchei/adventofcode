22176420336963d5395dc26cc6afcd94d0cc8193
theodorSchei <43009539+theodorSchei@users.noreply.github.com>
2024-12-03 22:13:20 +0100

    add some benchmarking

--- 2024/03/main.go
+++ 2024/03/main.go
@@ -6,6 +6,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func readLines(filePath string) ([]string, error) {
@@ -39,7 +40,6 @@ func Part2(lines []string) string {
 	for _, line := range lines {
 		res := r.FindAllStringSubmatch(line, -1)
 		for _, capture := range res {
-			println(capture[0])
 			if capture[0] == `do()` {
 				do = true
 				continue
@@ -59,7 +59,22 @@ func Part2(lines []string) string {
 }
 
 func main() {
+	start := time.Now()
 	lines, _ := readLines("./input.txt")
-	fmt.Println("Part 1: ", Part1(lines))
-	fmt.Println("Part 2: ", Part2(lines))
+	readTime := time.Since(start)
+
+	part1Start := time.Now()
+	part1 := Part1(lines)
+	part1Time := time.Since(part1Start)
+
+	part2Start := time.Now()
+	part2 := Part2(lines)
+	part2Time := time.Since(part2Start)
+
+	totalTime := time.Since(start)
+
+	fmt.Printf("Read %d lines in %s\n", len(lines), readTime)
+	fmt.Printf("Part 1: %-16s (%s)\n", part1, part1Time)
+	fmt.Printf("Part 2: %-16s (%s)\n", part2, part2Time)
+	fmt.Printf("Total: %s\n", totalTime)
 }
